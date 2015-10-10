aa9025dcdc83d1f611830ee4c18f182f0b790f20
Petar Shomov <petar@activitystream.com>
2015-10-10 00:57:02 +0000

    starting on the sample for Reducto

---
+++ src/Reducto.Sample.Tests/Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Reducto.Sample.Tests
+{
+    [TestFixture ()]
+    public class Test
+    {
+        [Test ()]
+        public void TestCase ()
+        {
+        }
+    }
+}
+
