e8d85a99e0fc3e9542437f80b2ffbc4765690953
Petar Shomov <petar@activitystream.com>
2015-11-19 21:56:37 +0000

    ref

--- src/Reducto.Sample/Infrastructure/CommandToAction.cs
+++ src/Reducto.Sample/Infrastructure/CommandToAction.cs
@@ -10,12 +10,12 @@ using System.Threading.Tasks;
 namespace Reducto.Sample
 {
     
-    public class StoreActionCommand<State> : ICommand
+    public class CommandToAction<State> : ICommand
     {
         Store<State> store;
         Func<Object> execute;
 
-        public StoreActionCommand(Store<State> store, Func<Object> execute)
+        public CommandToAction(Store<State> store, Func<Object> execute)
         {
             this.execute = execute;
             this.store = store;
@@ -34,12 +34,12 @@ namespace Reducto.Sample
         }
     }
 
-    public class StoreActionCommand<State, T> : ICommand
+    public class CommandToAction<State, T> : ICommand
     {
         Store<State> store;
         Func<T, Object> execute;
 
-        public StoreActionCommand(Store<State> store, Func<T, Object> execute)
+        public CommandToAction(Store<State> store, Func<T, Object> execute)
         {
             this.execute = execute;
             this.store = store;
@@ -58,12 +58,12 @@ namespace Reducto.Sample
         }
     }
 
-    public class StoreAsyncActionCommand<State> : ICommand
+    public class CommandToAsyncAction<State> : ICommand
     {
         Store<State> store;
         Func<Store<State>.AsyncAction> action;
 
-        public StoreAsyncActionCommand(Store<State> store, Func<Store<State>.AsyncAction> action)
+        public CommandToAsyncAction(Store<State> store, Func<Store<State>.AsyncAction> action)
         {
             this.action = action;
             this.store = store;
@@ -81,4 +81,23 @@ namespace Reducto.Sample
             store.Dispatch(action());
         }
     }
+
+    public static class ReductorMVVMExtensions
+    {
+        public static ICommand createActionCommand<State>(this Store<State> store, Func<Object> actionMaker)
+        {
+            return new CommandToAction<State>(store, actionMaker);
+        }
+
+        public static ICommand createActionCommand<State, T>(this Store<State> store, Func<T, Object> actionMaker)
+        {
+            return new CommandToAction<State, T>(store, actionMaker);
+        }
+
+        public static ICommand createAsyncActionCommand<State>(this Store<State> store, Func<Store<State>.AsyncAction> actionMaker)
+        {
+            return new CommandToAsyncAction<State>(store, actionMaker);
+        }
+    }
+
 }
